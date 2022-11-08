e4f14b5a50cd2c5ea5c8a74e39fafed7777688d2
Ваше Имя <you@example.com>
2022-11-08 22:17:33 +0600

    azat

--- knigi/views.py
+++ knigi/views.py
@@ -1,5 +1,24 @@
-from django.http import HttpResponse
+from rest_framework import generics
+
+from .models import *
+from .serializers import *
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class CategoryApiVew(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class Booksapiview(generics.ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+
+
+class AboutApiView(generics.ListAPIView):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+
+
+class HelpApiView(generics.ListAPIView):
+    queryset = Help.objects.all()
+    serializer_class = HelpSerializer
